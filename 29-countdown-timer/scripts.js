d2c0bb4bd838a18305097fd059d1f4b55e3f8d5f
awf825 <faiden454@gmail.com>
2019-11-03 10:01:40 -0500

    -Important Takeaways   -setInterval => https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals#setInterval()   -works like setTimeout except not for a one off function: instead   of clearing after the second argument (milliseconds) it keeps   firing at that interval and clearInterval() is called with an   identifying value   -Interval gets stored in a global variable (countdown) as not to run   infinitely; need to use clearInterval() instead of simply returning   the function

--- 29-countdown-timer/scripts.js
+++ 29-countdown-timer/scripts.js
@@ -1,8 +1,11 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left')
 const endTime = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
 
 function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -10,7 +13,7 @@ function timer(seconds) {
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     //check if we should stop it!
-    if(secondsLeft < 0) {
+    if(secondsLeft <= 0) {
       clearInterval(countdown);
       return;
     }
@@ -35,3 +38,16 @@ function displayEndTime(timestamp) {
   const minutes = end.getMinutes();
   endTime.textContent = `Be Back At ${adjustedHour}:${minutes}`;
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+});
